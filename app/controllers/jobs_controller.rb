34b50312097cfbbafd84ecba5d0ac3559fe3e844
Suryadev <suryadevmp@gmail.com>
2025-10-02 11:53:11 +0530

    Models

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,41 @@
+class JobsController < ApplicationController
+
+before_action :authenticate_user!
+before_action :set_job, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @jobs = current_user.jobs.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @job = current_user.jobs.build
+  end
+
+  def create
+    @job = current_user.jobs_build(job_params)
+    if @job.save   
+      redirect_to jobs_path, notice: "Job was successfully created."
+    else
+      render :new, alert: "There was an error creating the job."
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "Job update succesfully"
+    else
+      render :edit, alert: "failed to update job"
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path, notice: "Job was succed"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "jobs/Index"
+  get "jobs/show"
+  get "jobs/new"
+  get "jobs/create"
+  get "jobs/edit"
+  get "jobs/update"
+  get "jobs/destroy"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
