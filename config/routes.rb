56c23be2b24a282b2e81d99fde5d510c4d6f99af
Suryadev <suryadevmp@gmail.com>
2025-10-29 23:56:16 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get "dashboard/index"
+  get "dashboard/index", to: "dashboard#index"
   resources :jobs
   devise_for :users
   resource :profile, only: [:edit,:update]
   root "jobs#index"
   resources :resumes
-  get "dashboard/index"
   get "up" => "rails/health#show", as: :rails_health_check  
 end
