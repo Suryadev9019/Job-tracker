9f41a1e0679b864b6215a47c1d6369e328fea84b
Suryadev <suryadevmp@gmail.com>
2025-11-02 01:24:03 +0530

    Dashboard controller 1.1

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
+
   def index
+    @jobs_by_status = current_user.jobs.group(:status).count
+    @jobs_by_time = current_user.jobs
+                               .group_by { |job| job.created_at.to_date }
+                              .transform_values(&:count)
+
+    @total_jobs = current_user.jobs.count
+    @interview_jobs = current_user.jobs.where(status: "interview").count
+    @pending_jobs = current_user.jobs.where(status: "pending").count
   end
 end
